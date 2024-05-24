74ac0b025f92e68414b72e48ad496ec8cb66071c
TuyetTrinh0304 <2024802010239@student.tdmu.edu.vn>
2024-05-24 12:41:23 +0700

    first commit

---
+++ screens/Appointment.js
@@ -0,0 +1,138 @@
+import React, { useEffect, useState } from "react";
+import { View, Image, ScrollView, KeyboardAvoidingView, Platform } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { Button, Text, TextInput } from "react-native-paper";
+import DatePicker from "react-native-date-picker";
+import firestore from "@react-native-firebase/firestore";
+import { useMyContextProvider } from "../index";
+
+const Appointment = ({ navigation, route }) => {
+    const { service } = route.params || {};
+    const [datetime, setDatetime] = useState(new Date());
+    const [open, setOpen] = useState(false);
+    const [note, setNote] = useState('');
+    const [title, setTitle] = useState('');
+    const [controller, dispatch] = useMyContextProvider();
+    const { userLogin } = controller;
+    const [disableButton, setDisableButton] = useState(false);
+    const [disableTextInput, setDisableTextInput] = useState(false);
+    const [error, setError] = useState('');
+    const APPOINTMENTs = firestore().collection("Appointments");
+    const SERVICES = firestore().collection("Services");
+
+    useEffect(() => {
+        if (service && service.state === "Đã đặt") {
+            setDisableButton(true);
+            setDisableTextInput(true);
+        } else {
+            setDisableButton(false);
+            setDisableTextInput(false);
+        }
+    }, [service]);
+
+    const handleSubmit = () => {
+        const currentTime = new Date();
+        const timePlus10Minutes = new Date(currentTime.getTime() + 30 * 60000);
+        
+        if (datetime < timePlus10Minutes) {
+            setError("Ngày đặt không hợp lệ. Ngày đặt phải lớn hơn thời gian hiện tại 30 phút.");
+            return;
+        }
+        
+        const noteValue = note.trim() === '' ? 'Không' : note;
+
+        APPOINTMENTs.add({
+            title: service.title,
+            price: service.price,
+            serviceId: service.id,
+            datetime,
+            state: 'Đã đặt',
+            orderBy: `${userLogin.email}`,
+            orderInfo: `${userLogin.fullName} - ${userLogin.phone}`,
+            note: noteValue
+        })
+        .then(r => {
+            APPOINTMENTs.doc(r.id).update({ id: r.id });
+            SERVICES.doc(service.id).update({ 
+                state: 'Đã đặt',
+                datetime,
+                orderBy: `${userLogin.email}`,
+                orderInfo: `${userLogin.fullName} - ${userLogin.phone}`,
+                note: noteValue
+            });
+            navigation.navigate("Appointments");
+        })
+        .catch(error => {
+            console.error("Lỗi khi đặt bàn: ", error);
+        });
+    };
+
+    return (
+        <KeyboardAvoidingView
+            style={{ flex: 1 }}
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+            <ScrollView style={{ padding: 10 }}>
+                <View style={{ flexDirection: 'row' }}>
+                    <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Tên bàn: </Text>
+                    <Text style={{ fontSize: 20 }}>{service && service.title}</Text>
+                </View>
+                <View style={{ flexDirection: 'row' }}>
+                    <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Tạo bởi: </Text>
+                    <Text style={{ fontSize: 20 }}>{service && service.create}</Text>
+                </View>
+                <View style={{ flexDirection: 'row' }}>
+                    <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Giá: </Text>
+                    <Text style={{ fontSize: 20, color: "red" }}>{parseFloat(service && service.price).toLocaleString()} ₫</Text>
+                </View>
+                {service && service.image !== "" && (
+                    <View style={{ flexDirection: 'row' }}>
+                        <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Hình ảnh: </Text>
+                        <Image
+                            source={{ uri: service && service.image }}
+                            style={{ height: 300, width: '100%' }}
+                            resizeMode="contain"
+                        />
+                    </View>
+                )}
+                <View style={{ flexDirection: 'row' }}>
+                    <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Tình trạng: </Text>
+                    <Text style={{ fontSize: 20 }}>{service && service.state}</Text>
+                </View>
+                <DatePicker
+                    modal
+                    open={open}
+                    date={datetime}
+                    onConfirm={(date) => {
+                        setOpen(false);
+                        setDatetime(date);
+                        setError('');
+                    }}
+                    onCancel={() => {
+                        setOpen(false);
+                    }}
+                />
+                <TouchableOpacity
+                    onPress={() => setOpen(true)}
+                    style={{ flexDirection: "row", justifyContent: "space-between" }}>
+                    <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Chọn ngày đặt: </Text>
+                    <Text style={{ fontSize: 20 }}>{datetime.toDateString()}</Text>
+                </TouchableOpacity>
+                {error !== '' && (
+                    <Text style={{ color: 'red', marginTop: 10 }}>{error}</Text>
+                )}
+                <TextInput
+                    style={{ borderColor: 'gray', borderWidth: 1, marginTop: 10, padding: 5 }}
+                    placeholder="Ghi chú thêm"
+                    onChangeText={text => setNote(text)}
+                    value={note}
+                    editable={!disableTextInput}
+                />
+                <Button style={{ margin: 10 }} textColor="white" buttonColor="#ff555b" mode="contained" onPress={handleSubmit} disabled={disableButton}>
+                    {service && service.state === "Đã đặt" ? "Đã có người đặt" : "Đặt bàn"}
+                </Button>
+            </ScrollView>
+        </KeyboardAvoidingView>
+    );
+};
+
+export default Appointment;
---
+++ screens/Menu/AddNewMenu.js
@@ -0,0 +1,109 @@
+import React, { useState } from "react";
+import { View, Image, ScrollView, KeyboardAvoidingView, Platform } from "react-native";
+import { Text, TextInput, Button } from "react-native-paper";
+import firestore from "@react-native-firebase/firestore";
+import storage from "@react-native-firebase/storage";
+import ImagePicker from "react-native-image-crop-picker";
+import { useMyContextProvider } from "../../index";
+import { Picker } from "@react-native-picker/picker";
+
+const AddNewMenu = ({ navigation }) => {
+    const [controller] = useMyContextProvider();
+    const { userLogin } = controller;
+    const [imagePath, setImagePath] = useState("");
+    const [title, setTitle] = useState("");
+    const [price, setPrice] = useState("");
+    const [infoMenu, setInfoMenu] = useState("");
+    const [category, setCategory] = useState("Đồ uống");
+    const MENUS = firestore().collection("Menus");
+
+    const handleAddNewMenu = () => {
+        if (!title || !price || !infoMenu || !imagePath) {
+            alert("Please fill all the fields and add an image");
+            return;
+        }
+
+        MENUS.add({
+            title,
+            infoMenu,
+            price,
+            create: userLogin.email,
+            category,
+        })
+        .then(response => {
+            const refImage = storage().ref("/menus/" + response.id + ".png");
+            refImage.putFile(imagePath)
+            .then(() =>
+                refImage.getDownloadURL()
+                .then(link => {
+                    MENUS.doc(response.id).update({
+                        id: response.id,
+                        image: link
+                    });
+                    navigation.navigate("Services");
+                    navigation.navigate("Menu");
+                })
+            )
+            .catch(e => console.log(e.message));
+        });
+    };
+
+    const handleUploadImage = () => {
+        ImagePicker.openPicker({
+            mediaType: "photo",
+            width: 400,
+            height: 300,
+        })
+        .then(image => setImagePath(image.path))
+        .catch(e => console.log(e.message));
+    };
+
+    return (
+        <KeyboardAvoidingView style={{ flex: 1 }} behavior={Platform.OS === "ios" ? "padding" : "height"}>
+            <ScrollView style={{ padding: 10 }}>
+                <Button textColor="white" buttonColor="#ff555b" style={{ margin: 10 }} mode="contained" onPress={handleUploadImage}>
+                    Upload Image
+                </Button>
+                {imagePath !== "" && (
+                    <Image source={{ uri: imagePath }} style={{ height: 200 }} />
+                )}
+                <Text style={{ fontSize: 20, fontWeight: "bold" }}>Tên món (nước):</Text>
+                <TextInput
+                    placeholder="Trà sữa, bánh tráng trộn, v.v..."
+                    value={title}
+                    onChangeText={setTitle}
+                    style={{ marginBottom: 10, borderWidth: 1, borderColor: "#ccc" }}
+                />
+                <Text style={{ fontSize: 20, fontWeight: "bold" }}>Thông tin món ăn:</Text>
+                <TextInput
+                    placeholder="v.v..."
+                    value={infoMenu}
+                    onChangeText={setInfoMenu}
+                    style={{ marginBottom: 10, borderWidth: 1, borderColor: "#ccc" }}
+                />
+                <Text style={{ fontSize: 20, fontWeight: "bold" }}>Giá món ăn:</Text>
+                <TextInput
+                    placeholder="0"
+                    value={price}
+                    onChangeText={setPrice}
+                    keyboardType="numeric"
+                    style={{ marginBottom: 10, borderWidth: 1, borderColor: "#ccc" }}
+                />
+                <Text style={{ fontSize: 20, fontWeight: "bold" }}>Loại món ăn:</Text>
+                <Picker
+                    selectedValue={category}
+                    onValueChange={(itemValue) => setCategory(itemValue)}
+                    style={{ marginBottom: 10, borderWidth: 1, borderColor: "#ccc" }}
+                >
+                    <Picker.Item color="#FF8C00" label="Đồ ăn vặt" value="Đồ ăn vặt" />
+                    <Picker.Item color="#FF8C00" label="Đồ uống" value="Đồ uống" />
+                </Picker>
+                <Button buttonColor="#ff555b" textColor="white" mode="contained" onPress={handleAddNewMenu}>
+                    Thêm món ăn
+                </Button>
+            </ScrollView>
+        </KeyboardAvoidingView>
+    );
+};
+
+export default AddNewMenu;
